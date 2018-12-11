75a3929d02267754dc8d2aa2f18cb9f115bb2ce9
Hyuntak Cha <chaht01@gmail.com>
2018-12-11 22:08:13 +0900

    stable

--- index.js
+++ index.js
@@ -3,22 +3,44 @@ var http = require("http");
 var app = express();
 var server = http.createServer(app);
 var port = 3333;
+var chroma = require("chroma-js");
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.use(express.static(__dirname));
+var game_setting;
 server.listen(port, function() {
+  game_setting = setupGame();
   console.log(`listen on ${port}`);
 });
 
-var game_sockets = {};
-var controller_sockets = {};
+function setupGame() {
+  let circle_cnt = 5;
+  let ret = {
+    A: [],
+    B: []
+  };
+  for (let i = 0; i < circle_cnt; i++) {
+    left = Math.random() - 0.5;
+    top = (Math.random() * 440) / 400 - 440 / 400 / 2;
+    color = chroma.random().num();
+    ret.A.push({ left, top, color });
+  }
+  for (let i = 0; i < circle_cnt; i++) {
+    left = Math.random() - 0.5;
+    top = (Math.random() * 440) / 400 - 440 / 400 / 2;
+    color = chroma.random().num();
+    ret.B.push({ left, top, color });
+  }
+  return ret;
+}
+
 var io = require("socket.io").listen(server);
 
 io.sockets.on("connection", function(socket) {
-  socket.emit("ios_connected", "helloworld");
+  socket.emit("connected", game_setting);
 
   socket.on("down__ar__server", function(data) {
     console.log(data);
@@ -33,8 +55,12 @@ io.sockets.on("connection", function(socket) {
   });
 
   socket.on("circles__web__server", function(data) {
-    console.log(data);
-    socket.broadcast.emit("circles__server__ar", data); //mirroring
+    game_setting = data;
+    socket.broadcast.emit("circles__server__ar", data);
+  });
+  socket.on("circles__ar__server", function(data) {
+    game_setting = data;
+    socket.broadcast.emit("circles__server__web", data);
   });
 
   // socket.on("dragging", function(data) {
